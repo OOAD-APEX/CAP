90156748b3b66590f086c1dd529c5176724a96a8
ch996811 <ch996811@gmail.com>
2024-04-01 02:12:34 +0800

    change alarm icon and name

--- app/src/main/java/com/example/cap/ui/alarm/AlarmFragment.kt
+++ app/src/main/java/com/example/cap/ui/alarm/AlarmFragment.kt
@@ -1,4 +1,4 @@
-package com.example.cap.ui.dashboard
+package com.example.cap.ui.alarm
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cap.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class AlarmFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -23,7 +23,7 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(AlarmViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
--- app/src/main/java/com/example/cap/ui/alarm/AlarmViewModel.kt
+++ app/src/main/java/com/example/cap/ui/alarm/AlarmViewModel.kt
@@ -1,13 +1,14 @@
-package com.example.cap.ui.dashboard
+package com.example.cap.ui.alarm
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class AlarmViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Alarm"
     }
+
     val text: LiveData<String> = _text
 }
