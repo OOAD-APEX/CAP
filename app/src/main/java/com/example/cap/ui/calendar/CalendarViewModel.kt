40bf846b922dd696e355379e8997693d5469abe4
Gustavo <su3g4196ta8@gmail.com>
2024-04-16 23:50:02 +0800

    feat(calendar): complete basic calendar view

---
+++ app/src/main/java/com/example/cap/ui/calendar/CalendarViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.cap.ui.calendar
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalendarViewModel : ViewModel() {
+
+}
