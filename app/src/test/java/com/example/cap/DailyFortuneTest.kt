0bc5eb9be26ff9932a4ccbd78b164643f45b8b17
洪裔晟 <a1b2c33212000@gmail.com>
2024-04-26 00:39:26 +0800

    feat:daily fortune unit test done

---
+++ app/src/test/java/com/example/cap/DailyFortuneTest.kt
@@ -0,0 +1,20 @@
+package com.example.cap
+
+import com.example.cap.ui.fortune.DailyFortuneViewModel
+import org.junit.Assert.*
+import org.junit.Test
+
+class DailyFortuneTest {
+    @Test
+    fun testGetRandomFortuneColor() {
+        val dailyFortuneViewModel = DailyFortuneViewModel()
+        val fortuneColors = listOf("紅", "橘", "黃", "綠", "藍", "紫", "粉", "黑", "白")
+        assertTrue(fortuneColors.contains(dailyFortuneViewModel.getRandomFortuneColor()))
+    }
+    @Test
+    fun testGetRandomDailyFortune() {
+        val dailyFortuneViewModel = DailyFortuneViewModel()
+        val dailyFortunes = listOf("大吉", "中吉", "小吉", "普通")
+        assertTrue(dailyFortunes.contains(dailyFortuneViewModel.getRandomDailyFortune()))
+    }
+}
