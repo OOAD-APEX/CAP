a0cfa430ebdf9019becfd7fb34717b8d4fcc424e
ch996811 <ch996811@gmail.com>
2024-04-29 17:41:28 +0800

    feat(alarm): add test

---
+++ app/src/test/java/com/example/cap/domain/AlarmTest.kt
@@ -0,0 +1,67 @@
+package com.example.cap.domain
+
+import android.app.AlarmManager
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.robolectric.RobolectricTestRunner
+import java.util.Calendar
+
+@RunWith(RobolectricTestRunner::class)
+class AlarmTest {
+
+    @Mock
+    private lateinit var mockContext: Context
+
+    @Mock
+    private lateinit var mockCalendar: Calendar
+
+    @Mock
+    private lateinit var mockAlarmManager: AlarmManager
+
+    @Mock
+    private lateinit var mockNotificationManager: NotificationManager
+
+    private lateinit var alarm: Alarm
+
+    @Before
+    fun setUp() {
+        mockContext = mock(Context::class.java)
+        mockCalendar = mock(Calendar::class.java)
+        mockAlarmManager = mock(AlarmManager::class.java)
+        mockNotificationManager = mock(NotificationManager::class.java)
+        alarm = Alarm()
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun testSetAlarm() {
+        `when`(mockContext.getSystemService(Context.ALARM_SERVICE)).thenReturn(mockAlarmManager)
+        alarm.setAlarm(mockContext, mockCalendar)
+        verify(mockAlarmManager).setExact(anyInt(), anyLong(), any())
+    }
+
+    @Test
+    fun testCancelAlarm() {
+        alarm.cancelAlarm(mockContext)
+        verify(mockContext).sendBroadcast(any(Intent::class.java))
+    }
+
+
+    @Test
+    fun testStopRingtoneAndNotification() {
+        `when`(mockContext.getSystemService(Context.NOTIFICATION_SERVICE)).thenReturn(mockNotificationManager)
+        alarm.stopRingtoneAndNotification(mockContext)
+        verify(mockNotificationManager).cancel(anyInt())
+    }
+
+}
